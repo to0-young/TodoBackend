eebce6ff6ab72ceacd4b5f9698034313fa1a5c6a
to0-young <74.boyko@gmail.com>
2022-12-15 13:43:32 +0200

    add users validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
+  validates :first_name, presence: true, length: {minimum: 3, maximum: 30 }
+  validates :last_name, presence: true, length: {minimum: 3, maximum: 30 }
+  validates :email, presence: true, uniqueness:true, length: {minimum: 8 , maximum: 30 }
+  validates :password, presence: true
 end
