eac28f4a011bbe3140a6d74253fde6984b913294
to0-young <74.boyko@gmail.com>
2022-12-17 17:27:56 +0200

    add seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
-  validates :first_name, presence: true, length: {minimum: 3, maximum: 30 }
-  validates :last_name, presence: true, length: {minimum: 3, maximum: 30 }
-  validates :email, presence: true, uniqueness:true, length: {minimum: 8 , maximum: 30 }
-  validates :password, presence: true
+  has_secure_password
+
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 30 }
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-user = User.new(
-  first_name: "jonny",
-  last_name: "joo",
-  email: "Jonny@gmail.com",
-  password: "",
-  )
-user.save!
+require "faker"
+
+5.times do |n|
+  # user = User.new(
+  #   first_name: "jonny",
+  #   last_name: "joo",
+  #   email: "user_#{n}@gmail.com",
+  #   password: "123",
+  # )
+  # user.save!
+
+  # user = User.create!(
+  #   first_name: "jonny",
+  #   last_name: "joo",
+  #   email: "user_#{n}@gmail.com",
+  #   password: "123",
+  # )
+
+  user = User.create_with(
+    password: "123",
+    first_name: "jonny",
+    last_name: "joo",
+  ).find_or_create_by!(email: "user_#{n}@gmail.com")
+end
+
 
