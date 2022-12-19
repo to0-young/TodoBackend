99eea0e2a90fe16a855797d2dd161a9c36a08cd6
to0-young <74.boyko@gmail.com>
2022-12-19 19:05:55 +0200

    added extension to contrallers

--- Gemfile
+++ Gemfile
@@ -54,6 +54,7 @@ group :development, :test do
   gem "rspec-rails"
   gem "factory_bot_rails"
   gem "faker"
+  gem 'pry-rails'
 end
 
 group :development do
---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,35 @@
+class Api::V1::UsersController < ApplicationController
+  def index
+    users = User.all
+    render json: users
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user, except: :password
+  end
+
+  def create
+    user = User.new params.permit(:first_name, :last_name, :email, :password)
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors.messages }, status: 422
+    end
+  end
+  def update
+    user = User.find(params[:id])
+    if user.update params.permit(:first_name, :last_name)
+      render json: user, status: 200
+    else
+      render json: { errors: user.errors.messages }, status: 422
+    end
+
+
+  end
+  def destroy
+    user = user.find(params[:id])
+    user.destroy
+  end
+end
+
