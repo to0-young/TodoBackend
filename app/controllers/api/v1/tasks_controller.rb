1b85658f3078133b3a53a0c51f191a0a6eecd845
to0-young <74.boyko@gmail.com>
2023-01-16 11:45:20 +0200

    add validation task

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -4,5 +4,7 @@ class Api::V1::TasksController < ApplicationController
     tasks = Task.where(user_id: current_session.user_id)
     render json: tasks
   end
+  def create
 
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user
 
+  validates :title, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :priority, presence: true, length: { minimum: 1, maximum: 20 }
+  validates :due_date, presence: true, length: { minimum: 1, maximum: 40 }
 end
