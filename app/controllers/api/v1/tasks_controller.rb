3843d2e60bcab58d6eb4eb341b328b92ff17eb31
to0-young <74.boyko@gmail.com>
2023-01-16 19:42:40 +0200

    Add create & destroy tasks actions

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -2,9 +2,38 @@ class Api::V1::TasksController < ApplicationController
 
   def index
     tasks = Task.where(user_id: current_session.user_id)
-    render json: tasks
+    render json: ActiveModelSerializers::SerializableResource.new(tasks).to_json, status: :ok
   end
-  def create
 
+  def create
+    # task = current_session.user.tasks.new(params.permit(:title, :priority,  :description, :due_date))
+    # task = Task.new(params.permit(:title, :priority,  :description, :due_date).merge(user_id: current_session.user_id))
+    task = Task.new(params.permit(:title, :priority,  :description, :due_date))
+    task.user_id = current_session.user_id
+
+    if task.save
+      render json: task, status: :created
+    else
+      render json: { errors: task.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  # def update
+  #   task = Task.find(params[:id])
+  #   if task.update params.permit(:title, :priority, :dueDate )
+  #     render json: task, status: :ok
+  #   else
+  #     render json: { errors: task.errors.messages }, status: :unprocessable_entity
+  #   end
+  # end
+  #
+
+  def destroy
+    task = task.find(params[:id])
+    task.destroy
+    render json: ActiveModelSerializers::SerializableResource.new(task).to_json, status: :ok
   end
 end
+
+
+
