f666a51358b6d71c70323079006b01a7083ed827
to0-young <74.boyko@gmail.com>
2023-02-01 20:23:59 +0200

    add update recovery_token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,14 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= current_session&.user
+    @current_user ||= current_session&.user || authorized_user
+  end
+  def authorized_user
+    token = request.headers['Authorization']
+    return nil if token.nil?
+    binding.pry
+    decoded_token =  JWT.decode(token, Rails.application.credentials[:jwt_secret], true, { algorithm: 'HS256' })
+
   end
 
   def authenticate!
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, :tasks, :forget_passwords
-      resource :sessions
+      resource :sessions, :users
     end
   end
 end
