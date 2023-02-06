c797f046f7c70b5893c1b24774d8aaa7faf380af
to0-young <74.boyko@gmail.com>
2023-02-06 14:14:54 +0200

    refactor forget passwords

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,9 +6,9 @@ class UserMailer < ApplicationMailer
     exp_payload = { user_id: @user.id }
     recovery_token = JWT.encode(exp_payload, Rails.application.credentials[:jwt_secret], 'HS256')
     @front_end_url = Rails.application.credentials[:front_end_url]
-    @recovery_url  = "#{@front_end_url}/passwords/new?recovery_token=#{recovery_token}"
+    @recovery_url = "#{@front_end_url}/passwords/new?recovery_token=#{recovery_token}"
     @login_url = "#{@front_end_url}/login"
-    mail(to: @user.email, subject: 'Your password change request')
+    mail(to: @user.email, subject: 'Your password change request' )
   end
 end
 
