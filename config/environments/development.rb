dad3b8d7cca290d4aaff3943ea6e4e87a1cc7c31
to0-young <74.boyko@gmail.com>
2023-02-23 15:08:50 +0200

    add fix host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,6 +70,8 @@ Rails.application.configure do
       resource '*', credentials: true, headers: :any, methods: [:get, :post, :patch, :put, :delete]
     end
   end
+  config.hosts << "*"
+
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   # config.action.mailer.default_url_options = {:host => 'localhost:3000'}
