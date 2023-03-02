daf61d58aade1dc9be7f18762989952491504e33
to0-young <74.boyko@gmail.com>
2023-03-02 14:33:49 +0200

    add new fix test create

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Api::V1::UsersController, type: :controller do
   describe "#create" do
     it "creates a new user and returns a success response" do
-      expect(User.count).to eq(1)
+      expect(User.count).to eq(0)
       post :create, params: { first_name: "John", last_name: "Doe", email: "john@example.com", password: "password123" }
       expect(response).to have_http_status(:created)
     end
