71d493d2527cb1e27c70e4648580a78f6cd74f23
to0-young <74.boyko@gmail.com>
2023-03-07 19:37:21 +0200

    add fix test users & tasks

--- spec/controllers/api/v1/tasks_controller_spec.rb
+++ spec/controllers/api/v1/tasks_controller_spec.rb
@@ -12,14 +12,14 @@ RSpec.describe Api::V1::TasksController, type: :controller do
 
     it 'when success' do
       get :index
-      allow(subject).to receive(:current_session).and_return(session)
+      allow(subject).to receive(:current_session)
       expect(response).to have_http_status(200)
       expect(json[:tasks].length).to be(1)
       expect(json[:tasks][0][:id]).to be(task.id)
     end
 
     it "when unauthorized" do
-      allow(controller).to receive(:current_session).and_return(nil)
+      allow(controller).to receive(:current_session)
       get :index
       expect(response).to have_http_status(401)
     end
@@ -27,13 +27,13 @@ RSpec.describe Api::V1::TasksController, type: :controller do
     describe "#show" do
       it "returns the task when it exists" do
         get :show, params: { id: task.id }
-        expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(200)
         expect(json[:id]).to eq(task.id)
       end
 
       it 'returns a success response' do
         get :show, params: { id: task.id }
-        expect(response).to have_http_status(:ok)
+        expect(response).to have_http_status(200)
       end
 
       it 'returns JSON data for the task' do
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(ActionMailer::Base.deliveries.count).to eq(0)
     end
 
-    it "creates a new user and returns a 201 status code with valid parameters" do
+    it "creates a new user and returns a 422 status code with valid parameters" do
       post :create, params: user
       expect(response).to have_http_status(422)
       expect(User.count).to eq(0)
