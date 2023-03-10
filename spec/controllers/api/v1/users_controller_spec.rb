6090cb6a331d85091d7ec4732a6a6da7b43e499c
to0-young <74.boyko@gmail.com>
2023-03-10 15:03:57 +0200

    add new test

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,29 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  # describe "#create" do
-  #   let(:user) { { first_name: "John", last_name: "Doe", email: "john.doe@example.com", password: "password" } }
-  #
-  #   it "when validation error" do
-  #     post :create, params: user
-  #     expect(response).to have_http_status(422)
-  #   end
-  # end
+  describe "#create" do
+    let(:user) { { first_name: "John", last_name: "Doe", email: "john.doe@example.com", password: "password" } }
+
+    it "when users created" do
+      post :create, params: user
+      expect(response).to have_http_status(201)
+    end
+    
+  end
 
   describe "#update" do
-    let(:user) { create(:user, email_confirmed: false ) }
-    let(:session) { create(:session, user_id: user.id )}
+    let(:user) { create(:user, email_confirmed: false) }
+    let(:session) { create(:session, user_id: user.id) }
 
     before do
       allow(controller).to receive(:current_session).and_return(session)
     end
 
-    it "when 200 ok status" do
-      expect(user.email_confirmed).to eq(false)
-      patch :update, params: { id: user.id,  email_confirmed: true  }
-      expect(response).to have_http_status(200)
-      user.reload
-      expect(user.email_confirmed).to eq(true)
+    context "when validation ok" do
+      it "when 200 ok status" do
+        expect(user.email_confirmed).to eq(false)
+        patch :update, params: { id: user.id, email_confirmed: true }
+        expect(response).to have_http_status(200)
+        user.reload
+        expect(user.email_confirmed).to eq(true)
+      end
+    end
+
+    context "when validation error" do
+      it "when 422 status" do
+        patch :update, params: { id: user.id, password: "rr", password_confirmation: "dd" }
+        expect(response).to have_http_status(422)
+        expect(json[:errors][:password_confirmation]).not_to be_empty
+      end
+
+      context "when unauthenticated" do
+        it "when 401 status" do
+          allow(controller).to receive(:current_session)  # це мок сесії
+          patch :update, params: { id: user.id, password: "" }
+          expect(response).to have_http_status(401)
+        end
+      end
     end
   end
 end
