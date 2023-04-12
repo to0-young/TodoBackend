c70a275d2864c6fbf56d763712fa3dedaad19ac3
Yevhen <to0-young>
2023-04-12 17:28:08 +0300

    fix controller messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,17 +6,9 @@ class MessagesController < ApplicationController
     @messages = Message.all
   end
 
-  # GET /messages/1 or /messages/1.json
-  def show
-  end
-
-  # GET /messages/new
+    # GET /messages/new
   def new
     @message = Message.new
-  end
-
-  # GET /messages/1/edit
-  def edit
   end
 
   # POST /messages or /messages.json
@@ -25,37 +17,21 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /messages/1 or /messages/1.json
-  def update
-    respond_to do |format|
-      if @message.update(message_params)
-        format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
-        format.json { render :show, status: :ok, location: @message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /messages/1 or /messages/1.json
-  def destroy
-    @message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+  # def destroy
+  #   @message.destroy
+  #
+  #   respond_to do |format|
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
     # Use callbacks to share common setup or constraints between actions.
