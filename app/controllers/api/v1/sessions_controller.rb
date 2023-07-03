9983d27f3a869b4d2128a3726ba620162a3b6d95
Yevhen <to0-young>
2023-07-03 14:50:30 +0300

    add edit secure

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -31,7 +31,7 @@ class Api::V1::SessionsController < ApplicationController
     token = JWT.encode(exp_payload, Rails.application.credentials[:jwt_secret], 'HS256')
 
     # засетити цей токен в куки
-    cookies[:session] = { value: token, expires: 24.hours, domain: request.host, same_site: :none, secure: true }
+    cookies[:session] = { value: token, expires: 24.hours, domain: request.host, secure: false }
 
     # повернути 201 респонс із меседжом, що все добре
      render json: ActiveModelSerializers::SerializableResource.new(current_session).to_json, status: 201
