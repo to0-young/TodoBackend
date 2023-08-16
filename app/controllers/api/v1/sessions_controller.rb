c82a210f28abf61d0921d07722bdcc7570b16872
Yevgeny <71845331+to0-young@users.noreply.github.com>
2023-08-16 12:40:20 +0300

    Revert "Revert "Todoback""

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -32,6 +32,7 @@ class Api::V1::SessionsController < ApplicationController
 
     # засетити цей токен в куки
 
+
     # cookies[:session] = { value: token, expires: 24.hours, domain: request.host, secure: Rails.application.credentials[:jwt_secure] }
     cookies[:session] = { value: token, expires: 24.hours, domain: request.host, secure: Rails.application.credentials[:jwt_secure] }
 
