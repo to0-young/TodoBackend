1c7d19205ccb9f7d6a741b982e95db441a6a14f4
Yevhen <to0-young>
2023-08-17 11:16:28 +0300

    edit cookies

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::SessionsController < ApplicationController
 
     # засетити цей токен в куки
     cookies[:session] = { value: token, expires: 24.hours, domain: request.host, secure: Rails.application.credentials[:jwt_secure] }
-        # cookies[:session] = { value: token, expires: 24.hours, same_site: :none, secure: Rails.application.credentials[:jwt_secure] }
+    # cookies[:session] = { value: token, expires: 24.hours, same_site: :none, secure: Rails.application.credentials[:jwt_secure] }
 
     # повернути 201 респонс із меседжом, що все добре
      render json: ActiveModelSerializers::SerializableResource.new(current_session).to_json, status: 201
