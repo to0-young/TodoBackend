9101143d8591f1040bd4d541a87fbb23363ebb29
Yevhen <to0-young>
2023-10-26 12:12:27 +0300

    add fix ActiveAdm

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,6 +2,10 @@ class Session < ApplicationRecord
   validates :expiration , presence: true
 
   belongs_to :user
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "expiration", "id", "updated_at", "user_id"]
+  end
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,12 @@ class User < ApplicationRecord
   validates :first_name, presence: true, length: { minimum: 1, maximum: 15 }
   validates :last_name, presence: true, length: { minimum: 1, maximum: 15 }
   validates :email, presence: true, uniqueness: true, length: { minimum: 8, maximum: 30 }
-end
 
+  def self.ransackable_attributes(auth_object = nil)
+    ["avatar", "created_at", "email", "email_confirmed", "first_name", "id", "last_name", "password_digest", "updated_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["sessions", "tasks", "message"] # додайте всі асоціації, які ви хочете використовувати для пошуку
+  end
+end
