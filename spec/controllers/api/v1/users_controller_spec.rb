849edd15e758f5e9a2b624e7f7f6f5474c01361a
Yevhen <to0-young>
2023-12-01 13:13:30 +0200

    add new test

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,18 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
   describe "#create" do
-    let(:valid_user_params) { { first_name: "John", last_name: "Doe", email: "john.doe@example.com", password: "password" } }
-    let(:invalid_user_params) { { first_name: "John", last_name: "Doe", email: "", password: "password" } }
+    let(:user) { { first_name: "John", last_name: "Doe", email: "john.doe@example.com", password: "password" } }
 
-    it "creates a new user with valid params" do
+    it "when users created" do
       expect(User.count).to eq(0)
-      post :create, params: { user: valid_user_params }
+      post :create, params: user
       expect(response).to have_http_status(201)
       expect(User.count).to eq(1)
     end
 
-    it "returns unprocessable entity with invalid params" do
-      post :create, params: { user: invalid_user_params }
+    it "When validation error" do
+      post :create, params: { first_name: "John", last_name: "Doe", email: "", password: "password" }
       expect(response).to have_http_status(422)
       expect(json[:errors]).not_to be_empty
     end
@@ -24,32 +23,32 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     let(:session) { create(:session, user_id: user.id) }
 
     before do
-      allow(controller).to receive(:current_user).and_return(user)
+      allow(controller).to receive(:current_session).and_return(session)
     end
 
-    context "with valid params" do
-      it "updates the user and returns ok status" do
+    context "when validation ok" do
+      it "when 200 ok status" do
         expect(user.email_confirmed).to eq(false)
-        patch :update, params: { user: { email_confirmed: true } }
+        patch :update, params: { id: user.id, email_confirmed: true }
         expect(response).to have_http_status(200)
         user.reload
         expect(user.email_confirmed).to eq(true)
       end
     end
 
-    context "with invalid params" do
-      it "returns unprocessable entity status" do
-        patch :update, params: { user: { password: "rr", password_confirmation: "dd" } }
+    context "when validation error" do
+      it "when 422 status" do
+        patch :update, params: { id: user.id, password: "rr", password_confirmation: "dd" }
         expect(response).to have_http_status(422)
         expect(json[:errors][:password_confirmation]).not_to be_empty
       end
-    end
 
-    context "when unauthenticated" do
-      it "returns unauthorized status" do
-        allow(controller).to receive(:current_user)
-        patch :update, params: { user: { password: "" } }
-        expect(response).to have_http_status(401)
+      context "when unauthenticated" do
+        it "when 401 status" do
+          allow(controller).to receive(:current_session)  # це мок сесії
+          patch :update, params: { id: user.id, password: "" }
+          expect(response).to have_http_status(401)
+        end
       end
     end
   end
