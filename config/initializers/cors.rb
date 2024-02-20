246447894bd5c640be7b3f33d02f63e707f9f000
Yevhen <to0-young>
2024-02-20 17:36:13 +0200

    add fix google for login

---
+++ config/initializers/cors.rb
@@ -0,0 +1,15 @@
+Rails.application.configure do
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'http://localhost:3001'
+      resource '*', credentials: true, headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    end
+  end
+  config.hosts << "*"
+  config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins Rails.application.credentials[:front_end_url] || ''
+      resource '*', credentials: true, headers: :any, methods: [:get, :post, :patch, :put, :delete]
+    end
+  end
+end
